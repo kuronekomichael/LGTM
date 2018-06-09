6dfabc0851f53140967374cc98705aad0bd3be71
kuronekomichael <hardwarehacking@gmail.com>
2018-06-09 20:08:48 +0900

    Fix: enter at "Submit your review" as well

--- content/content.js
+++ content/content.js
@@ -1,5 +1,12 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    var oldMessage = $("textarea[name='comment[body]']").val();
-    $("textarea[name='comment[body]']").val(oldMessage + "\n\n" + request.image);
+    [
+      "textarea[name='comment[body]']",
+      "textarea[name='pull_request_review[body]']"
+    ].forEach(function(query) {
+      var oldMessage = $(query).val();
+      $(query).val(
+        (oldMessage ? oldMessage + "\n\n" : '') + request.image
+      );
+    });
 });
